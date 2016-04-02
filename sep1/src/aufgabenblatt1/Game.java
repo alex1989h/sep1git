f0a64b70bae5092c4812a9a1788b666e61c71c3a
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2016-04-02 21:40:11 +0200

    Von uns bearbeited

--- sep1/src/aufgabenblatt1/Game.java
+++ sep1/src/aufgabenblatt1/Game.java
@@ -3,9 +3,9 @@ package aufgabenblatt1;
 public class Game {
   public StringBuffer board;
 
-  public Game(String s) {
-    board = new StringBuffer(s);
-  }
+  private static final int NoMove = -1;
+  
+  public Game(String s) {board = new StringBuffer(s);}
 
   public Game(StringBuffer s, int position, char player) {
     board = new StringBuffer();
@@ -14,35 +14,33 @@ public class Game {
   }
 
   public int move(char player) {
-    for (int i = 0; i < 9; i++) {
-      if (board.charAt(i) == '-') {
-        Game game = play(i, player);
-        if (game.winner() == player)
-          return i;
+    int defaultMove = NoMove;
+    for (int move = 0; move < 9; move++) {
+      if (feldIstLeer(move)){ 
+        defaultMove = move;
+        Game game = play(move, player);
+        if (game.winner() == player) 
+          defaultMove = move;
       }
     }
-
-    for (int i = 0; i < 9; i++) {
-      if (board.charAt(i) == '-')
-        return i;
-    }
-    return -1;
+    return defaultMove;
   }
 
-  public Game play(int i, char player) {
-    return new Game(this.board, i, player);
+  public Game play(int move, char player) {
+    return new Game(this.board, move, player);
   }
 
   public char winner() {
-    if (board.charAt(0) != '-' && board.charAt(0) == board.charAt(1)
-        && board.charAt(1) == board.charAt(2))
-      return board.charAt(0);
-    if (board.charAt(3) != '-' && board.charAt(3) == board.charAt(4)
-        && board.charAt(4) == board.charAt(5))
-      return board.charAt(3);
-    if (board.charAt(6) != '-' && board.charAt(6) == board.charAt(7)
-        && board.charAt(7) == board.charAt(8))
-      return board.charAt(6);
+    for (int move = 0; move < 7; move+=3) {
+      if (!feldIstLeer(move) 
+            && board.charAt(move) == board.charAt(move+1) 
+            && board.charAt(move+1) == board.charAt(move+2))
+      return board.charAt(move);
+    }
     return '-';
+  }
+  
+  public boolean feldIstLeer(int move){
+    return board.charAt(move)=='-';
   }
 }
